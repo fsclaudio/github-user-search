a8f91823cb6b69d26eca8971b224e5929fdd73b8
fsclaudio <fsclaudio@gmail.com>
2021-02-26 22:03:13 -0300

    Add Navbr

---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./styles.css";
+
+const Navbar=() => (
+ <nav className="main-navbar">
+    <a href="Home" className="nav-text">Bootcamp DevSuperior </a> 
+ </nav>   
+) ;
+
+export default Navbar;
